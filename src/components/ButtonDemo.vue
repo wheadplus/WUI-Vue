72dee2601ce0fb76ca70ebed331dfc39416af2a4
“whx” <386058251@qq.com>
2022-07-06 16:03:03 +0800

    新增button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
   Button 示例
+  <Button :loading="true">ss</Button>
   </div>
 </template>
+
+<script lang="ts">
+//@ignore
+import Button from '../lib/Button.vue'
+export default {
+  components: {
+    Button
+  }
+}
+</script>
